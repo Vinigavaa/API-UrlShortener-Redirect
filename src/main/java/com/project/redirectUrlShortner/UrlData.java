d48c0fcf7f9e28cca08dda2604841c4f64efc035
Zer0nights <137710927+Zer0nights@users.noreply.github.com>
2024-11-23 16:34:32 -0300

    Finished

---
+++ src/main/java/com/project/redirectUrlShortner/UrlData.java
@@ -0,0 +1,15 @@
+package com.project.redirectUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
